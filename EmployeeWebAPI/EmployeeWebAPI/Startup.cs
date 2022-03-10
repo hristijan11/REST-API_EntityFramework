14e395fb1b2aa160711bcb52fd7cd511f5af0fa4
hristijan_j <45484589+hristijan11@users.noreply.github.com>
2022-03-10 01:26:19 -0800

    Updating the code

--- EmployeeWebAPI/EmployeeWebAPI/Startup.cs
+++ EmployeeWebAPI/EmployeeWebAPI/Startup.cs
@@ -28,7 +28,6 @@ namespace EmployeeWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddControllers();
             services.AddMvc();
             services.AddDbContext<EmployeeDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultAPI")));
 
